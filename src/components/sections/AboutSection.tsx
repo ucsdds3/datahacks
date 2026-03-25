bca04e00aa732ea605f94eecd9ab5d00ff28712b
Ansh Bhatnagar <anshbhatnagar1612@gmail.com>
2026-03-24 22:37:02 -0700

    logo fix

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -18,7 +18,7 @@ const AboutSection = () => {
     coOrganizers: [
       {
         name: "BRAIN",
-        logo: "/club-logos/brain.png",
+        logo: "/club-logos/BRAIN.png",
         url: "",
       },
       {
